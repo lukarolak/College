8a10ebdab414ea8442e1a770f04584e7f2131d9d
Luka Rolak <lukarolak2@gmail.com>
2017-10-23 15:32:14 +0200

    Add files via upload

---
+++ Zad1.c
@@ -0,0 +1,73 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+
+void prvi(){
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a > b)
+		printf("%d \n", a);
+	else
+		printf("%d \n", b);
+	if (a % 2 == 0)
+		printf("a je paran \n");
+	else
+		printf("a je neparan \n", a);
+	if (b % 2 == 0)
+		printf("b je paran \n");
+	else
+		printf("b je neparan \n", a);
+	if (a > 0)
+		printf("a je veci od nule \n");
+	if (b > 0)
+		printf("b je veci od nule \n");
+	getch();
+
+}
+
+void drugi(){
+	char znak = getch();
+	if ((znak >= 'a' && znak <= 'z' ) || (znak >= 'A' && znak <= 'Z')){
+		printf("slovo je");
+		if (znak >= 'a' && znak <= 'z')
+			printf(" malo ");
+		else
+			printf(" veliko ");
+
+	}
+	else 
+		if (znak >= '0' && znak <= '9')
+			printf(" broj je ");
+		else
+		printf(" znak je ");
+	getch();
+}
+
+void treci(){
+	float a, b, c;
+	scanf("%f %f %f", &a, &b, &c);
+	float max = a;
+	float minn = a;
+	if (b > max)
+		max = b;
+	if (c > max)
+		max = c;
+	printf("Najveci broj je %f", max);
+	if (abs)
+
+
+}
+
+int main(){
+	drugi();
+
+
+
+
+
+
+
+
+	return 0;
+}
